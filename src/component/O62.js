ddcd8ab819686978b1a82cf3d3d99f0aa8747c8a
yeha <aheynoez@gmail.com>
2022-08-17 01:17:42 +0900

    c

--- src/component/O62.js
+++ src/component/O62.js
@@ -1,6 +1,11 @@
 import './O62.css';
+import React, { useState } from 'react';
 
 function O62() {
+    let [page, pageChage] = useState(1);
+    let [lastPage, lastPageChage] = useState(31);
+    let [left, leftChage] = useState(false);
+    let [right, rightChage] = useState(true);
     return (
         <div className="O62">
             <div className="background062">
@@ -9,16 +14,55 @@ function O62() {
                         <div className="xline1062"></div>
                         <div className="xline2062"></div>
                     </div>
-                    <div className="leftBtn062">
-                        <div className="leftline1062"></div>
-                        <div className="leftline2062"></div>
-                    </div>
+
+                    {left && (
+                        <div
+                            className="leftBtn062"
+                            onClick={() => {
+                                pageChage(page - 1);
+
+                                if (page === 2) {
+                                    leftChage(false);
+                                }
+                                if (page > 2) {
+                                    leftChage(true);
+                                }
+                                if (page < lastPage + 1) {
+                                    rightChage(true);
+                                }
+                            }}
+                        >
+                            <div className="leftline1062"></div>
+                            <div className="leftline2062"></div>
+                        </div>
+                    )}
+
                     <div className="letterbox062">
                         <textarea className="note062"></textarea>
                     </div>
-                    <div className="rightBtn062">
-                        <div className="rightline1062"></div>
-                        <div className="rightline2062"></div>
+
+                    {right && (
+                        <div
+                            className="rightBtn062"
+                            onClick={() => {
+                                pageChage(page + 1);
+
+                                if (page > 0) {
+                                    leftChage(true);
+                                }
+                                if (page === lastPage - 1) {
+                                    rightChage(false);
+                                }
+                            }}
+                        >
+                            <div className="rightline1062"></div>
+                            <div className="rightline2062"></div>
+                        </div>
+                    )}
+                    <div>
+                        <h1>
+                            {page}/{lastPage}
+                        </h1>
                     </div>
                 </div>
             </div>
